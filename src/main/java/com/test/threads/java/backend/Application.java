c81453f2d1f8c9186f431eced7f7fbb8f6d7be72
Pavan Fernando <pavanfernando6@gmail.com>
2025-10-31 09:50:18 +0530

    done changes

--- src/main/java/com/test/threads/java/backend/Application.java
+++ src/main/java/com/test/threads/java/backend/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 @EnableAsync
 public class Application {
 
